ec254350f019f920cc29eef4732a993c68b2334e
Arashidani <Asarashi@live.jp>
2024-03-30 11:18:42 +0900

    不要なコメントを削除

--- lib/features/app/side_drawer.dart
+++ lib/features/app/side_drawer.dart
@@ -8,27 +8,6 @@ class SideDrawer extends StatelessWidget {
     return Container(
       child: ListView(
         children: [
-          Row(
-            children: [
-              Expanded(
-                  flex: 1, // 1割
-                  child: IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.close),
-                  )),
-              const Expanded(
-                flex: 8, // 8割
-                child: Text(
-                  'Your Text',
-                  style: TextStyle(fontSize: 20),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              Expanded(
-                  flex: 1, // 1割
-                  child: Container()),
-            ],
-          ),
           const UserAccountsDrawerHeader(
             accountName: Text('John Doe'),
             accountEmail: Text('john.doe@example.com'),
